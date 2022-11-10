1c8ba625b15d77e9cd7e76e2985c6d26cef4dcb1
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-11-10 16:37:19 -0500

    moved HTML into index.js file

--- index.js
+++ index.js
@@ -4,25 +4,56 @@ const fs = require(`fs`);
 let employeeList = [];
 
 const addEmployee = () => {
+
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <title>Team Profiles</title>
+</head>
+<body>
+    <header>My Team</header>  
+    <section>
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+            <h5 class="card-title">${managerName} || ${employeeName}</h5> // Can I do this?????? 
+            <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+            <ul class="card-text">
+                <li>${id}</li> 
+                <li>${email}</li> 
+                <li>${officeNumber}</li> 
+                <li>${gitHub}</li> 
+                <li>${school}</li> 
+            </ul>
+            </div>
+        </div>
+    </section>
+</body>
+</html>`
+
 inquirer.prompt([
 {
     type: "input",
-    name: "managerName",
+    name: "managerName", //the directions here don't make sense to me 
     message: "What is the team manager's name?",
 },
 {
     type: "input",
-    name: "employeeId",
+    name: "id",
     message: "What is the employee ID?",
 },
 {
     type: "input",
-    name: "employeeEmail",
+    name: "email",
     message: "What is the employee email address?",
 },
 {
     type: "input",
-    name: "employeeOffice",
+    name: "officeNumber",
     message: "What is the employee office number?",
 },
 ]
--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,5 +1,13 @@
 const Employee = require("../lib/employee");
 
-describe("employee", () => {
-    it("should return employee ")
-})
+describe(`employee`, () => {
+    it("should return employee name, id, email, role", () => {
+        
+    const obj = new Employee;
+
+    expect("name" in obj).toEqual(true);
+    expect("id" in obj).toEqual(true);
+    expect("email" in obj).toEqual(true);
+    expect("role" in obj).toEqual(Employee);
+});
+});
