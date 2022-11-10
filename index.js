3e04b9400a2589f9751cac7d6e611c264191a0bd
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-11-10 18:29:54 -0500

    Updated index file functions; updated employee and manager js files; updated employee test file

--- index.js
+++ index.js
@@ -1,69 +1,101 @@
 const inquirer = require(`inquirer`);
 const fs = require(`fs`);
+const Manager = require("./lib/manager");
 
 let employeeList = [];
 
-const addEmployee = () => {
+// `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+//   <link rel="stylesheet" href="style.css">
+//   <title>Team Profiles</title>
+//   <style>
+//     header {
+//         text-align: center;
+//         background-color: blueviolet;
+//         color: white;
+//         font-weight: bold;
+//         font-size: x-large;
+//         height: 50px;
+//     }
+//   </style>
+// </head>
+// <body>
+//     <header>My Team</header>  
+//     <section>
+//         <div class="card" style="width: 18rem;">
+//             <div class="card-body">
+//             <h5 class="card-title">${managerName} || ${employeeName}</h5> // Can I do this?????? 
+//             <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+//             <ul class="card-text">
+//                 <li>${id}</li> 
+//                 <li>${email}</li> 
+//                 <li>${officeNumber}</li> 
+//                 <li>${gitHub}</li> 
+//                 <li>${school}</li> 
+//             </ul>
+//             </div>
+//         </div>
+//     </section>
+// </body>
+// </html>`
 
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
-  <title>Team Profiles</title>
-</head>
-<body>
-    <header>My Team</header>  
-    <section>
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-            <h5 class="card-title">${managerName} || ${employeeName}</h5> // Can I do this?????? 
-            <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
-            <ul class="card-text">
-                <li>${id}</li> 
-                <li>${email}</li> 
-                <li>${officeNumber}</li> 
-                <li>${gitHub}</li> 
-                <li>${school}</li> 
-            </ul>
-            </div>
-        </div>
-    </section>
-</body>
-</html>`
+addManager = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the team manager's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the employee ID?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the employee email address?",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the employee office number?",
+        },
+    ])
 
-inquirer.prompt([
-{
-    type: "input",
-    name: "managerName", //the directions here don't make sense to me 
-    message: "What is the team manager's name?",
-},
-{
-    type: "input",
-    name: "id",
-    message: "What is the employee ID?",
-},
-{
-    type: "input",
-    name: "email",
-    message: "What is the employee email address?",
-},
-{
-    type: "input",
-    name: "officeNumber",
-    message: "What is the employee office number?",
-},
-]
-)
+        .then(answers => {
+            const newManager = new Manager(answers.id, answers.managerName, answers.email, answers.officeNumber)
+            employeeList.push(newManager);
+            console.log(answers);
+            menu();
+        })
+};
+
+
+addEmployee = () => {
+    inquirer.prompt([ 
+    {
+        type: "list",
+        name: "employeeType",
+        message: "Select an employee type?",
+        choices: ["Engineer", "Intern"],
+}])
 .then(answers => {
-    employeeList.push;
-    console.log(answers);
-    menu();
+    if("Engineer" === answers.employeeType) {
+        addEnginner()
+    }
+    else { addIntern()
+    }
 })
 }
+
+//add enginner - do the same thing as on 71-75
+//add intern - do the same thing as on 71-75
 
 const print = () => {
     console.log("printing");
@@ -72,28 +104,31 @@ const print = () => {
 
 const exit = () => {
     console.log("Byyyye");
-    return; 
+    return;
 }
 
 
-function menu (){
-return inquirer.prompt([
-    {
-    type: "list",
-    message: "Choose an option",
-    choices: ["Add an employee", "Print", "Exit"]
-    }
-])
-.then( ({option}) => {
-    switch(option){
-        case "Add an employee":
-            return addEmployee();
-        case "Print":
-            return print();
-        case "Exit":
-            return exit(); 
-    }
-});
+function menu() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "mainMenu",
+            message: "Choose an option",
+            choices: ["Add an employee", "Print", "Exit"]
+        }
+    ])
+        .then((answers) => {
+            switch (answers.mainMenu) {
+                case "Add an employee":
+                    return addEmployee();
+                case "Print":
+                    return print();
+                case "Exit":
+                    return exit();
+            }
+        });
 }
 
-menu();
+// menu();
+
+addManager();
--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,13 +1,41 @@
 const Employee = require("../lib/employee");
 
 describe(`employee`, () => {
-    it("should return employee name, id, email, role", () => {
+    it("should return employee name", () => {
+        
+    const obj = new Employee("123", "Sam", "Sam@test.com");
+
+    expect(obj.name).toEqual("Sam");
+ 
+});
+});
+
+describe(`employee`, () => {
+    it("should return employee id", () => {
         
     const obj = new Employee;
 
-    expect("name" in obj).toEqual(true);
-    expect("id" in obj).toEqual(true);
+    expect("id" in obj).toEqual(true); ///do what you did above
+});
+});
+
+describe(`employee`, () => {
+    it("should return employee email", () => {
+        
+    const obj = new Employee;
+
     expect("email" in obj).toEqual(true);
-    expect("role" in obj).toEqual(Employee);
+
 });
 });
+
+describe(`employee`, () => {
+    it("should return employee role", () => {
+        
+    const obj = new Employee;
+
+    expect("role" in obj).toEqual(true);
+});
+});
+
+//need to add tests for the indiviudal functions 
